4f27c548be406fc55bec853ba291251d33572486
pratik <pratikpalashikar@gmail.com>
2018-07-04 12:52:23 -0400

    Adding the test cases, added the maven surefire plugin

---
+++ src/test/java/com/techmisal/JewelsNStonesTest.java
@@ -0,0 +1,31 @@
+package com.techmisal;
+
+
+
+import com.techmisal.easy.JewelsNStones;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class JewelsNStonesTest {
+
+
+
+    private JewelsNStones jewelsNStones = new JewelsNStones();
+
+
+
+    @Test
+    public void testJewelsNStones(){
+
+
+        Assert.assertEquals(3,jewelsNStones.numJewelsInStones("aA","aAAbbbb"));
+        Assert.assertEquals(0,jewelsNStones.numJewelsInStones("z","ZZ"));
+
+
+    }
+
+
+
+}
