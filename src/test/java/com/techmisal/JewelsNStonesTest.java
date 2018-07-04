0a5fa2ac9c23a3e4d8be228a588f62ad9a7bf281
pratik <pratikpalashikar@gmail.com>
2018-07-04 12:57:47 -0400

    formatting the code

--- src/test/java/com/techmisal/JewelsNStonesTest.java
+++ src/test/java/com/techmisal/JewelsNStonesTest.java
@@ -1,31 +1,26 @@
 package com.techmisal;
 
 
-
-import com.techmisal.easy.JewelsNStones;
-import org.junit.Assert;
-import org.junit.Test;
-
+        import com.techmisal.easy.JewelsNStones;
+        import org.junit.Assert;
+        import org.junit.Test;
 
 
 public class JewelsNStonesTest {
 
 
-
     private JewelsNStones jewelsNStones = new JewelsNStones();
 
 
-
     @Test
-    public void testJewelsNStones(){
+    public void testJewelsNStones() {
 
 
-        Assert.assertEquals(3,jewelsNStones.numJewelsInStones("aA","aAAbbbb"));
-        Assert.assertEquals(0,jewelsNStones.numJewelsInStones("z","ZZ"));
+        Assert.assertEquals(3, jewelsNStones.numJewelsInStones("aA", "aAAbbbb"));
+        Assert.assertEquals(0, jewelsNStones.numJewelsInStones("z", "ZZ"));
 
 
     }
-
 
 
 }
