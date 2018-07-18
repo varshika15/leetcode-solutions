51c31a10a1a19d312d61b16093a8fc22faa0114b
pratik <pratikpalashikar@gmail.com>
2018-07-17 23:39:35 -0400

    Adding the easy solution for implmenting lowercase logic

---
+++ src/main/java/com/techmisal/easy/LowerCase.java
@@ -0,0 +1,26 @@
+package com.techmisal.easy;
+
+public class LowerCase {
+
+
+    public String toLowerCase(String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        char c = ' ';
+        for (int i = 0; i < str.length(); i++) {
+            int j = (str.charAt(i) - 'A');
+            if (j > -1 && j < 27) {
+                c = (char) (j + 97);
+            } else {
+                c = str.charAt(i);
+            }
+            sb.append("" + c);
+        }
+
+
+        return sb.toString();
+
+    }
+
+}
