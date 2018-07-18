e8b5cf36810c49bb0dc029e844932d3d7ea27ea8
pratik <pratikpalashikar@gmail.com>
2018-07-17 23:40:43 -0400

    Adding the easy solution for implmenting lowercase logic

--- src/main/java/com/techmisal/easy/LowerCase.java
+++ src/main/java/com/techmisal/easy/LowerCase.java
@@ -1,5 +1,28 @@
 package com.techmisal.easy;
 
+/*
+
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+
+
+
+
+ */
+
 public class LowerCase {
 
 
