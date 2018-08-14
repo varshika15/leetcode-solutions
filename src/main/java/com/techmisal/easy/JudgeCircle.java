afe595205da33121d9ae0a15b1938da4008969f9
pratik <pratikpalashikar@gmail.com>
2018-08-13 23:29:53 -0400

    Commiting JudgeCircle problem statement solution

---
+++ src/main/java/com/techmisal/easy/JudgeCircle.java
@@ -0,0 +1,55 @@
+package com.techmisal.easy;
+
+/*
+
+    Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+
+The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle.
+
+Example 1:
+Input: "UD"
+Output: true
+Example 2:
+Input: "LL"
+Output: false
+
+
+ */
+
+
+
+public class JudgeCircle {
+
+
+    public boolean judgeCircleMethod(String moves) {
+
+
+
+        int X=0;
+        int Y=0;
+
+
+        for(int i=0;i<moves.length();i++){
+
+           if(moves.charAt(i)=='U')
+                X++;
+            if(moves.charAt(i)=='D')
+                X--;
+            if(moves.charAt(i)=='L')
+                Y--;
+            else
+                Y++;
+
+        }
+
+        if(X==0 && Y==0){
+            return true;
+        }
+
+
+        return false;
+
+    }
+
+
+}
